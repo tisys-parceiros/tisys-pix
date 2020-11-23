31cf10ce031279e02c0099318f498f87505797d7
tisys <74541757+tisys-parceiros@users.noreply.github.com>
2020-11-23 19:37:49 -0300

    Update HelloCommando.java

--- src/main/java/org/acme/commandmode/HelloCommando.java
+++ src/main/java/org/acme/commandmode/HelloCommando.java
@@ -1,9 +1,5 @@
 package org.acme.commandmode;
 
-import javax.enterprise.context.control.ActivateRequestContext;
-import javax.inject.Inject;
-
-import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
